6bb90c2b20dfc132f29642b82a09d3058862e59c
Sanka.Weerakoon <snkwrkn@gmail.com>
2019-04-14 19:30:46 +0530

    hacker rank ipk JumpingOnTheClouds

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import ipk.JumpingOnTheClouds;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Jumping on the clouds
+       int[] arr = new int[]{0,0,0,1,0,0};
+
+        System.out.println(JumpingOnTheClouds.jumpingOnClouds(arr));
+
+
+    }
+}
